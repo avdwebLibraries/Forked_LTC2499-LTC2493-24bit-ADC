d5fcff6a857cf37801addee8658d36adcf2ca6cb
Albert van Dalen <albertfora@avdweb.nl>
2020-02-28 13:38:53 +0100

    Add files via upload

--- LTC2499.cpp
+++ LTC2499.cpp
@@ -25,7 +25,7 @@ LTC2499: 8/16 channel 24bit ADC
 
 This library contains the ADC part of the combined library Ard2499 which contains the libraries for the LTC2499 24bit ADC and the 24AA025E48 EEPROM
 See the 16-Channel 24-Bit ADC Data Acquisition Shield for Arduino from Iowa Scaled Engineering:
-* https://www.iascaled.com/store/Arduino/ArduinoDataAcquisition/ARD-
+* https://www.iascaled.com/store/ARD-LTC2499
 * GitHub https://github.com/IowaScaledEngineering/ard-ltc2499
 
 Changelog
